b003363c0a7dfd848e1475d8aa4ca39d757f7316
Lars Leo Grätz <23726273+LarsLeo@users.noreply.github.com>
2021-05-01 13:35:51 +0200

    Initial Commit

---
+++ CMonkeh/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CMonkeh
+//
+//  Created by Lars Graetz on 01.05.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
