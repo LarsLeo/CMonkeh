5db19622764aad71c6f74f995fa6f814d2b96264
Lars Leo Grätz <23726273+LarsLeo@users.noreply.github.com>
2021-05-02 13:56:18 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,63 @@
+//
+//  main.cpp
+//  CMonkeh
+//  The main class wrapping glfw and creating a window with an OpenGL context.
+//
+//  Created by Lars Graetz on 01.05.21.
+//
+
+#include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(1280, 800, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    int gladInitRes = gladLoadGL();
+    if (!gladInitRes) {
+        std::cout << "Unable to initialize glad\n" << std::endl;
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+
+    std::cout << "Running with OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+        
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
